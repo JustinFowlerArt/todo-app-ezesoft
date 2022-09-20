1d0a11f38acfc14612dfaa4ff6d304dfbd5c99dd
Justin Fowler <jusfowle@gmail.com>
2022-09-20 12:40:19 -0400

    Initial commit

---
+++ client/src/app.tsx
@@ -0,0 +1,3 @@
+const App = () => <div>I am an app!</div>;
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,66 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const config = {
+    mode: isProd ? 'production' : 'development',
+    stats: 'errors-only',
+    entry: {
+        index: './src/index.tsx',
+    },
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+        }),
+
+        new CopyPlugin({
+            patterns: [{ from: 'src/images', to: 'images' }],
+        }),
+    ],
+};
+
+if (isProd) {
+    config.optimization = {
+        minimizer: [new TerserWebpackPlugin()],
+    };
+} else {
+    config.devServer = {
+        port: 9000,
+        open: true,
+        hot: true,
+        compress: true,
+        client: {
+            overlay: {
+                errors: true,
+                warnings: false,
+            },
+        },
+    };
+}
+
+module.exports = config;
